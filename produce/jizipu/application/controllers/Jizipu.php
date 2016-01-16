7ec13d5bc5be5df4deba0160b067451b4fe60647
codekissyoung <cky951010@163.com>
2016-01-16 15:55:12 +0800

    gitpush

--- produce/jizipu/application/controllers/Jizipu.php
+++ produce/jizipu/application/controllers/Jizipu.php
@@ -6,6 +6,7 @@ class Jizipu extends CI_Controller{
 	public function index(){
 //		$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx57c3018bd11713e4&secret=2dda6ad80590e404df1901ddd4238f33';
 // 		$a = file_get_contents($url);
+		$this->load->database();
 		$query = $this->db->query('select * from city');
 		foreach ($query->result() as $row)
 		{
