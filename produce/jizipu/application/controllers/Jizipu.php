b25e7502ff702732841dc292c557d1e92d31d281
codekissyoung <cky951010@163.com>
2016-01-16 14:51:36 +0800

    gitpush

--- produce/jizipu/application/controllers/Jizipu.php
+++ produce/jizipu/application/controllers/Jizipu.php
@@ -4,15 +4,6 @@ class Jizipu extends CI_Controller{
 		parent::__construct();
 	}
 	public function index(){
-		debug($_GET);
-		$tamp 	   = $_GET['timestamp'];
-		$nonce	   = $_GET['nonce'];
-		$token	   = 'codekissyoung';
-		$array     = array($timestamp,$nonce,$token);
-		sort($array,SORT_STRING);
-		$tmpstr = implode($array);
-		$tmpstr = sha1($tmpstr);
-		debug($tmpstr,'tmpstr');
 		echo $_GET['echostr'];
 	}
 }
