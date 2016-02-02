d1b9b6f27f5dd2501679e7fcdd33a91b2c6826f6
codekissyoung <cky951010@163.com>
2016-02-02 20:02:09 +0800

    gitpush

--- produce/jizipu/application/controllers/Jizipu.php
+++ produce/jizipu/application/controllers/Jizipu.php
@@ -179,5 +179,34 @@ class Jizipu extends CI_Controller{
 	
 	public function test(){
 		echo "test!!!!!";
+		
+		$model = 'F2LPH9FQG5QV';
+		$curl = curl_init();
+		
+		curl_setopt_array($curl, array(
+				CURLOPT_URL => "http://a.apix.cn/3023/apple/model.html?model=$model",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_HTTPHEADER => array(
+						"accept: application/json",
+						"apix-key:08330c6405aa466e4dca33144655ce06",
+						"content-type: application/json"
+				),
+		));
+		
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		
+		curl_close($curl);
+		
+		if ($err) {
+			echo "cURL Error #:" . $err;
+		} else {
+			var_dump($response);
+		}
 	}
 }
